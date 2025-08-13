7dae45945eecb8912fdab7e21d74582f3a1adfea
vitor melo <vitor.melo@sempreceub.com.br>
2025-08-13 09:57:17 -0300

    fix: adjusts and configs chacra ui

--- app/alunos/page.js
+++ app/alunos/page.js
@@ -1,52 +1,56 @@
-import styles from "../page.module.css";
-import Link from 'next/link';
+
+import {LayoutAdmin} from '../../layout'
+
 
 export default function Alunos() {
   return (
-    <div className={styles.page}>
-      <aside className={styles.aside}>
-        <h1>Adm GEM</h1>
-        <ul className={styles.ul}>
-          <li className={styles.li}>
-            <Link href="./alunos">Alunos</Link>
-          </li>
-          <li className={styles.li}>
-            <Link href="./instrutores">Instrutores</Link>
-          </li>
-        </ul>
-      </aside>
-      <main className={styles.main}>
-        <nav className={styles.nav}>
-          <h2>Alunos</h2>
-          <a href="#" className={styles.caixaUsuario}>Usuário</a>
-        </nav>
-        <section className={styles.section}>
-          <div className={styles.containerBtns}>
-            <button className={styles.btns}>Nova Lição</button>
-            <button className={styles.btns}>Novo Aluno</button>
-          </div>
-          <table className={styles.table}>
-            <thead>
-              <tr className={styles.tr}>
-                <th className={styles.th}>Nome</th>
-                <th className={styles.th}>Instrumento</th>
-                <th className={styles.th}>Nível</th>
-                <th className={styles.th}>Lições Aprovadas</th>
-              </tr>
+    <LayoutAdmin>
+    asdasdasd
+    </LayoutAdmin>
+    // <div className={styles.page}>
+    //   <aside className={styles.aside}>
+    //     <h1>Adm GEM</h1>
+    //     <ul className={styles.ul}>
+    //       <li className={styles.li}>
+    //         <Link href="./alunos">Alunos</Link>
+    //       </li>
+    //       <li className={styles.li}>
+    //         <Link href="./instrutores">Instrutores</Link>
+    //       </li>
+    //     </ul>
+    //   </aside>
+    //   <main className={styles.main}>
+    //     <nav className={styles.nav}>
+    //       <h2>Alunos</h2>
+    //       <a href="#" className={styles.caixaUsuario}>Usuário</a>
+    //     </nav>
+    //     <section className={styles.section}>
+    //       <div className={styles.containerBtns}>
+    //         <button className={styles.btns}>Nova Lição</button>
+    //         <button className={styles.btns}>Novo Aluno</button>
+    //       </div>
+    //       <table className={styles.table}>
+    //         <thead>
+    //           <tr className={styles.tr}>
+    //             <th className={styles.th}>Nome</th>
+    //             <th className={styles.th}>Instrumento</th>
+    //             <th className={styles.th}>Nível</th>
+    //             <th className={styles.th}>Lições Aprovadas</th>
+    //           </tr>
 
-            </thead>
-            <tbody>
-              <tr className="styles.tr">
-                <td className={styles.td}>Alfredo Futterkiste</td>
-                <td className={styles.td}>Violino</td>
-                <td className={styles.td}>RJM</td>
-                <td className={styles.td}><Link href="./licoes">Visualizar Histórico</Link></td>
-              </tr>
+    //         </thead>
+    //         <tbody>
+    //           <tr className="styles.tr">
+    //             <td className={styles.td}>Alfredo Futterkiste</td>
+    //             <td className={styles.td}>Violino</td>
+    //             <td className={styles.td}>RJM</td>
+    //             <td className={styles.td}><Link href="./licoes">Visualizar Histórico</Link></td>
+    //           </tr>
 
-            </tbody>
-          </table>
-        </section>
-      </main>
-    </div>
+    //         </tbody>
+    //       </table>
+    //     </section>
+    //   </main>
+    // </div>
   );
 }
